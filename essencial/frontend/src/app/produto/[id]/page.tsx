9349991056b8c55557c10c8655a66b8a2f652404
Luana <rezende_luana.31@hotmail.com>
2024-10-06 22:13:20 -0300

    Incluindo projeto

---
+++ essencial/frontend/src/app/produto/[id]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import useProdutos from "@/data/hooks/useProdutos"
+
+
+export default function PaginaProduto(props:any){
+    const {produtos } =  useProdutos()
+    
+    const id = +props.params.id
+    const produto = produtos.find((p) => p.id === id)
+    return produto ? <div>{produto?.nome}</div> : <div>Produto não encontrado</div>
+
+
+}
